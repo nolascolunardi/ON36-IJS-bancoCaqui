2d490f52094c3b9872a6840ec1cdce4baff434dc
Nolasco <nolascolunardi@gmail.com>
2024-07-26 23:19:51 -0300

    adicionado ao gerente funcionalidades de cadastrar/deletar clientes e abrir/alterar/fechar contas
    Foi adicionado as funcionalidades de cadastrar e deletar clientes, além de poder abrir, alterar tipo e fechar contas para clientes cadastrados no sistema.

--- src/gerentes/gerentes.controller.ts
+++ src/gerentes/gerentes.controller.ts
@@ -6,48 +6,57 @@ import { GerentesService } from './gerentes.service';
 
 @Controller('gerentes')
 export class GerentesController {
+  constructor(private readonly GerentesService: GerentesService) {}
 
-    constructor(private readonly GerentesService: GerentesService ) {}
+  @Post('/clientes')
+  createCliente(
+    @Body('nome') nome: string,
+    @Body('cpf') cpf: string,
+    @Body('email') email: string,
+    @Body('telefone') telefone: string,
+    @Body('dataNasc') dataNasc: string,
+    @Body('endereco') endereco: string,
+  ): Cliente {
+    return this.GerentesService.createCliente(
+      nome,
+      cpf,
+      email,
+      telefone,
+      dataNasc,
+      endereco,
+    );
+  }
 
-    @Post('/clientes')
-    createCliente(
-        @Body('nome') nome: string,
-        @Body('cpf') cpf: string, 
-        @Body('email') email: string, 
-        @Body('telefone')telefone: string, 
-        @Body('dataNasc') dataNasc: string, 
-        @Body('endereco') endereco: string
-        ): Cliente {
-            return this.GerentesService.createCliente(nome, cpf, email, telefone, dataNasc, endereco);
-        }
+  @Delete('/clientes/:id')
+  removeCliente(@Param('id') id: number): void {
+    return this.GerentesService.removeCliente(id);
+  }
 
-    @Delete('/clientes/:id')
-    removeCliente(@Param('id') id: number): void {
-        return this.GerentesService.removeCliente(id);
-    }
+  @Post('/clientes/criarConta')
+  createConta(
+    @Body('idCliente') idCliente: number,
+    @Body('numeroConta') numeroConta: string,
+    @Body('tipoConta') tipoConta: TipoConta,
+    @Body('saldo') saldo: number,
+  ) {
+    return this.GerentesService.createConta(
+      idCliente,
+      numeroConta,
+      tipoConta,
+      saldo,
+    );
+  }
 
-    @Post('cliente/criarConta')
-    createConta(
-        @Body('idCliente') idCliente: number,
-        @Body('numeroConta') numeroConta: string,
-        @Body('tipoConta') tipoConta: TipoConta,
-        @Body('saldo') saldo: number
-    ) {
-        return this.GerentesService.createConta(idCliente, numeroConta, tipoConta, saldo);
-    }
-    
+  @Patch('/clientes/alterarConta/:idCliente')
+  updateTipoConta(
+    @Param('idCliente') idCliente: number,
+    @Body('tipoConta') tipoConta: TipoConta,
+  ) {
+    return this.GerentesService.updateTipo(idCliente, tipoConta);
+  }
 
-    // @Patch('cliente/alterarTipo/:idCliente')
-    // updateTipoConta(
-    //     @Param('idCliente') idCliente: number,
-    //     @Body('tipoConta') tipoConta: TipoConta
-    // ) {
-    //     return this.GerentesService.updateTipo(idCliente, tipoConta);
-    // }
-
-    // @Patch('cliente/:idCliente')
-    // closeConta(@Param('idCliente') idCliente: number): void {
-    //     return this.GerentesService.closeConta(idCliente);
-    // }
-
+  @Delete('/clientes/deletarConta/:idCliente')
+  closeConta(@Param('idCliente') idCliente: number): void {
+    return this.GerentesService.closeConta(idCliente);
+  }
 }
--- src/gerentes/gerentes.service.ts
+++ src/gerentes/gerentes.service.ts
@@ -7,111 +7,136 @@ import * as path from 'path';
 
 @Injectable()
 export class GerentesService {
+  private readonly filePathCliente = path.resolve('src/clientes/clientes.json');
+  private readonly filePathConta = path.resolve('src/contas/contas.json');
 
-    private readonly filePathCliente = path.resolve('src/clientes/clientes.json');
-    private readonly filePathConta = path.resolve('src/contas/contas.json');
+  private readClientes(): Cliente[] {
+    const data = fs.readFileSync(this.filePathCliente, 'utf-8');
+    return JSON.parse(data) as Cliente[];
+  }
+  private writeClientes(clientes: Cliente[]): void {
+    fs.writeFileSync(
+      this.filePathCliente,
+      JSON.stringify(clientes, null, 2),
+      'utf-8',
+    );
+  }
+  private readContas(): Conta[] {
+    const data = fs.readFileSync(this.filePathConta, 'utf-8');
+    return JSON.parse(data) as Conta[];
+  }
+  private writeContas(contas: Conta[]): void {
+    fs.writeFileSync(
+      this.filePathConta,
+      JSON.stringify(contas, null, 2),
+      'utf-8',
+    );
+  }
 
-    private readClientes(): Cliente[]{
-        const data = fs.readFileSync(this.filePathCliente, 'utf-8'); 
-        return JSON.parse(data) as Cliente[];
-    }
-    private writeClientes(clientes: Cliente[]): void{
-        fs.writeFileSync(this.filePathCliente, JSON.stringify(clientes, null, 2), 'utf-8');
-    }
-    private readContas(): Conta[]{
-        const data = fs.readFileSync(this.filePathConta, 'utf-8'); 
-        return JSON.parse(data) as Conta[];
-    }
-    private writeContas(contas: Conta[]): void{
-        fs.writeFileSync(this.filePathConta, JSON.stringify(contas, null, 2), 'utf-8');
-    }
+  // requisitos relacionado a clientes
+  createCliente(
+    nome: string,
+    cpf: string,
+    email: string,
+    telefone: string,
+    dataNasc: string,
+    endereco: string,
+  ): Cliente {
+    const usuarios = this.readClientes();
+    const newCliente = {
+      idCliente:
+        usuarios.length > 0 ? usuarios[usuarios.length - 1].idCliente + 1 : 1,
+      nome,
+      cpf,
+      email,
+      telefone,
+      dataNasc,
+      endereco,
+    };
+    usuarios.push(newCliente);
+    this.writeClientes(usuarios);
+    return newCliente;
+  }
 
-    // requisitos relacionado a clientes
-    createCliente(nome: string, cpf: string, email: string, telefone: string, dataNasc: string, endereco: string): Cliente {
+  removeCliente(id: number): void {
+    const clientes = this.readClientes();
+    const index = clientes.findIndex(
+      (cliente) => cliente.idCliente === Number(id),
+    );
 
-        const usuarios = this.readClientes();
-        const newCliente = {
-            idCliente: usuarios.length > 0 ? usuarios[usuarios.length - 1].idCliente + 1 : 1,
-            nome,
-            cpf,
-            email,
-            telefone,
-            dataNasc,
-            endereco
-        };
-        usuarios.push(newCliente);
-        this.writeClientes(usuarios);
-        return newCliente;
-
-    }
-
-    removeCliente(id: number): void{
-        const clientes = this.readClientes();
-        const index = clientes.findIndex(cliente => cliente.idCliente === Number(id));
-        
-        if(index === -1){
-            console.log(`Cliente com o id ${id} não encontrado.`);
-        }
-
-        clientes.splice(index, 1);
-        console.log(`Cliente com o id ${id} removido com sucesso.`);
-        this.writeClientes(clientes);
-    }
-
-    // requisitos relacionado a cont
-    createConta(idCliente: number, numeroConta: string, tipoConta: TipoConta, saldo: number ): Conta {
-        const cliente = this.findByIdCliente(idCliente);
-        const contas = this.readContas();
-
-        if(!cliente){
-            return;
-        }
-
-        const newConta = {
-            idCliente,
-            idConta: contas.length > 0 ? contas[contas.length - 1].idConta + 1 : 1 ,
-            numeroConta,
-            tipoConta,
-            status: true,
-            saldo
-        }
-        contas.push(newConta);
-        this.writeContas(contas);
-        return newConta;
+    if (index === -1) {
+      console.log(`Cliente com o id ${id} não encontrado.`);
     }
 
-    // updateTipo(idCliente: number, newTipo: TipoConta): Conta{
-    //     const contas = this.readContas();
-    //     const conta = contas.find(conta => conta.idCliente === Number(idCliente));
-        
-    //     if(!conta){
-    //         console.log(`Conta com o id ${idCliente} não encontrada.`);
-    //     }
+    clientes.splice(index, 1);
+    console.log(`Cliente com o id ${id} removido com sucesso.`);
+    this.writeClientes(clientes);
+  }
 
-    //     conta.tipoConta = newTipo;
-    //     this.writeContas(contas);
-    //     return conta;
-    // }
+  // requisitos relacionado a cont
+  createConta(
+    idCliente: number,
+    numeroConta: string,
+    tipoConta: TipoConta,
+    saldo: number,
+  ): Conta {
+    const cliente = this.findByIdCliente(idCliente);
+    const contas = this.readContas();
 
-    // closeConta(id: number): void{
-    //     const contas = this.readContas();
-    //     const conta = contas.find(conta => conta.idCliente === Number(id));
-        
-    //     if(!conta){
-    //         console.log(`Conta com o id ${id} não encontrada.`);
-    //     }
-    //     conta.status = false;
-    //     this.writeContas(contas);
-    // }
-    
-    findByIdCliente(id: number): Cliente{
-        const clientes = this.readClientes();
-        const cliente = clientes.find(cliente => cliente.idCliente === Number(id));
-        
-        if(!cliente){
-            console.log(`Cliente com o id ${id} não encontrado.`);
-        }
-        return cliente;
+    if (!cliente) {
+      return;
     }
 
+    const newConta = {
+      idCliente,
+      idConta: contas.length > 0 ? contas[contas.length - 1].idConta + 1 : 1,
+      numeroConta,
+      tipoConta,
+      status: true,
+      saldo,
+    };
+    contas.push(newConta);
+    this.writeContas(contas);
+    return newConta;
+  }
+
+  updateTipo(idCliente: number, newTipo: TipoConta): Conta {
+    const contas = this.readContas();
+    const conta = contas.find((conta) => conta.idCliente === Number(idCliente));
+
+    if (!conta) {
+      console.log(`Conta com o id ${idCliente} não encontrada.`);
+    }
+
+    conta.tipoConta = newTipo;
+    this.writeContas(contas);
+    return conta;
+  }
+
+  closeConta(id: number): void {
+    const contas = this.readContas();
+    const contaIndex = contas.findIndex(
+      (contas) => contas.idCliente === Number(id),
+    );
+
+    if (contaIndex === -1) {
+      console.log(`Conta com o id ${id} não encontrada.`);
+    }
+
+    contas.splice(contaIndex, 1);
+    console.log(`Conta com o id ${id} removida com sucesso.`);
+    this.writeContas(contas);
+  }
+
+  findByIdCliente(id: number): Cliente {
+    const clientes = this.readClientes();
+    const cliente = clientes.find(
+      (cliente) => cliente.idCliente === Number(id),
+    );
+
+    if (!cliente) {
+      console.log(`Cliente com o id ${id} não encontrado.`);
+    }
+    return cliente;
+  }
 }
