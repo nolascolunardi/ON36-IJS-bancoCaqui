ddcd5e1136ef111f540f80c9c03461f2b5df6f7e
nolasco <nolascolunardi@gmail.com>
2024-07-27 18:01:39 -0300

    Adicionado ao module principal os imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GerentesModule } from './gerentes/gerentes.module';
+import { ClientesModule } from './clientes/clientes.module';
+import { ContasModule } from './contas/contas.module';
 
 @Module({
-  imports: [],
+  imports: [GerentesModule, ClientesModule, ContasModule],
   controllers: [AppController],
   providers: [AppService],
 })
