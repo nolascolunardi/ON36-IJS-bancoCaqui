5cf425b9d282b853fa27b4cd09239db1ce66294f
Nolasco <nolascolunardi@gmail.com>
2024-07-26 23:15:43 -0300

    adicionado ao cliente funcionalidades de abrir, alterar tipo e fechar conta

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -1,6 +1,37 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { TipoConta } from '../contas/contas.model';
+import { ClienteService } from './clientes.service';
 
 @Controller('clientes')
 export class ClientesController {
+  constructor(private readonly ClientesService: ClienteService) {}
 
+  @Post('/abrirConta')
+  createConta(
+    @Body('idCliente') idCliente: number,
+    @Body('numeroConta') numeroConta: string,
+    @Body('tipoConta') tipoConta: TipoConta,
+    @Body('saldo') saldo: number,
+  ) {
+    return this.ClientesService.createConta(
+      idCliente,
+      numeroConta,
+      tipoConta,
+      saldo,
+    );
+  }
+
+  @Patch('/alterarConta/:idCliente')
+  updateTipoConta(
+    @Param('idCliente') idCliente: number,
+    @Body('tipoConta') tipoConta: TipoConta,
+  ) {
+    return this.ClientesService.updateTipo(idCliente, tipoConta);
+  }
+
+  @Delete('/fecharConta/:idCliente')
+  closeConta(@Param('idCliente') idCliente: number): void {
+    return this.ClientesService.closeConta(idCliente);
+  }
 }
--- src/clientes/clientes.service.ts
+++ src/clientes/clientes.service.ts
@@ -1,6 +1,100 @@
 import { Injectable } from '@nestjs/common';
+import { Cliente } from '../clientes/clientes.model';
+import { Conta } from '../contas/contas.model';
+import { TipoConta } from '../contas/contas.model';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class ClienteService {
+  private readonly filePathCliente = path.resolve('src/clientes/clientes.json');
+  private readonly filePathConta = path.resolve('src/contas/contas.json');
 
+  private readClientes(): Cliente[] {
+    const data = fs.readFileSync(this.filePathCliente, 'utf-8');
+    return JSON.parse(data) as Cliente[];
+  }
+  private writeClientes(clientes: Cliente[]): void {
+    fs.writeFileSync(
+      this.filePathCliente,
+      JSON.stringify(clientes, null, 2),
+      'utf-8',
+    );
+  }
+  private readContas(): Conta[] {
+    const data = fs.readFileSync(this.filePathConta, 'utf-8');
+    return JSON.parse(data) as Conta[];
+  }
+  private writeContas(contas: Conta[]): void {
+    fs.writeFileSync(
+      this.filePathConta,
+      JSON.stringify(contas, null, 2),
+      'utf-8',
+    );
+  }
+  createConta(
+    idCliente: number,
+    numeroConta: string,
+    tipoConta: TipoConta,
+    saldo: number,
+  ): Conta {
+    const cliente = this.findByIdCliente(idCliente);
+    const contas = this.readContas();
+
+    if (!cliente) {
+      return;
+    }
+
+    const newConta = {
+      idCliente,
+      idConta: contas.length > 0 ? contas[contas.length - 1].idConta + 1 : 1,
+      numeroConta,
+      tipoConta,
+      status: true,
+      saldo,
+    };
+    contas.push(newConta);
+    this.writeContas(contas);
+    return newConta;
+  }
+
+  updateTipo(idCliente: number, newTipo: TipoConta): Conta {
+    const contas = this.readContas();
+    const conta = contas.find((conta) => conta.idCliente === Number(idCliente));
+
+    if (!conta) {
+      console.log(`Conta com o id ${idCliente} não encontrada.`);
+    }
+
+    conta.tipoConta = newTipo;
+    this.writeContas(contas);
+    return conta;
+  }
+
+  closeConta(id: number): void {
+    const contas = this.readContas();
+    const contaIndex = contas.findIndex(
+      (contas) => contas.idCliente === Number(id),
+    );
+
+    if (contaIndex === -1) {
+      console.log(`Conta com o id ${id} não encontrada.`);
+    }
+
+    contas.splice(contaIndex, 1);
+    console.log(`Conta com o id ${id} removida com sucesso.`);
+    this.writeContas(contas);
+  }
+
+  findByIdCliente(id: number): Cliente {
+    const clientes = this.readClientes();
+    const cliente = clientes.find(
+      (cliente) => cliente.idCliente === Number(id),
+    );
+
+    if (!cliente) {
+      console.log(`Cliente com o id ${id} não encontrado.`);
+    }
+    return cliente;
+  }
 }
