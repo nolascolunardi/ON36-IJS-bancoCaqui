2d896171b6127c78c61b3b1f091d760d741d4268
Ana Laura Nolasco <nolascolunardi@gmail.com>
2024-09-05 00:01:46 -0300

    refactor: classe usuario modificada

--- src/usuarios/usuarios.ts
+++ src/usuarios/usuarios.ts
@@ -3,13 +3,11 @@ export abstract class Usuario {
   public cpf: string;
   public email: string;
   public telefone: string;
-  public cep: string;
 
-  constructor(nome: string, cpf: string, email: string, telefone: string, cep: string) {
+  constructor(nome: string, cpf: string, email: string, telefone: string) {
     this.nome = nome;
     this.cpf = cpf;
     this.email = email;
     this.telefone = telefone;
-    this.cep = cep;
   }
 }
